7bb609681bfce71cd13d0f7a8d98b179f396b25e
Mattia Righetti <matt95.righetti@gmail.com>
2020-08-25 16:55:44 +0200

    Made Job variables public for test purposes

--- src/main/java/Server/Job.java
+++ src/main/java/Server/Job.java
@@ -3,8 +3,8 @@ package Server;
 import java.util.concurrent.Callable;
 
 public class Job implements Callable<String> {
-    private final String jobId;
-    private final int milliseconds;
+    public final String jobId;
+    public final int milliseconds;
 
     public Job (String jobId, int milliseconds) {
         this.jobId = jobId;
