a09200c509b93870d15b8885333fbd5cfe2ac0b8
Tallino <gdanese@hotmail.it>
2020-08-28 13:18:09 +0200

    created ObjectSerializer

---
+++ common/src/main/java/ds/common/FaultTolerance/ObjectSerializer.java
@@ -0,0 +1,42 @@
+package ds.common.FaultTolerance;
+
+import java.io.*;
+import java.nio.file.Path;
+
+public class ObjectSerializer implements Serializable {
+    Path path;
+    String filepath;
+
+    public ObjectSerializer() {
+        filepath = path.toString();
+    }
+
+    public <T> void writeObjToFile(T objectToSerialize) {
+        try {
+            FileOutputStream fileOut = new FileOutputStream(filepath);
+            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+            objectOut.writeObject(objectToSerialize);
+            objectOut.close();
+            System.out.println("The Object was succesfully written to a file");
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public <T> T readObjFromFile() {
+        try {
+            FileInputStream fileIn = new FileInputStream(filepath);
+            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+            T obj = (T) objectIn.readObject();
+            System.out.println("The Object has been read from the file");
+            objectIn.close();
+            return obj;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+
+    }
+}
